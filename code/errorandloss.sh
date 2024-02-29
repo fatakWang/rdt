7c7251f27369e8badc0b3a52926201a1076d31b6
fatak <3020001267@tju.edu.cn>
2024-02-29 23:32:16 +0800

    first commit

---
+++ code/errorandloss.sh
@@ -0,0 +1,4 @@
+#./prog_t <msg_num> <loss> <corruption> <snd_time> <trace> <runi>
+./a 100 0.5 0.5 10 5 1
+# ./GBN 20 0.0 0.0 1000 2 2
+# ./GBN 30 0.0 0.3 1000 2 3
---
+++ t&t/run_sample.sh
@@ -0,0 +1,4 @@
+#./prog_t <msg_num> <loss> <corruption> <snd_time> <trace> <runi>
+./a 10 0.0 0.0 100 2 1
+# ./GBN 20 0.0 0.0 1000 2 2
+# ./GBN 30 0.0 0.3 1000 2 3
